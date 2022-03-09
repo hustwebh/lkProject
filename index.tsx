9a313159c7433cb2e41dbf777cb433bd2df66cbf
Mr.quit <76393033+hustwebh@users.noreply.github.com>
2022-03-09 17:43:26 +0800

    Add files via upload
    退出登录

---
+++ index.tsx
@@ -0,0 +1,21 @@
+import { connect } from 'dva';
+import style from './index.less';
+
+interface mainContentProps {
+  MainPage: any;
+  // wappers: any;
+}
+
+const MainContent: any = (props: any) => {
+  return <div className={style.rightContent}>各个菜单界面</div>;
+};
+
+// MainContent.wrappers = ['@/wrappers/auth'];
+
+const mapStateToProps = ({ mainPage }: { mainPage: any }) => {
+  return {
+    MainPage: mainPage,
+  };
+};
+
+export default connect(mapStateToProps)(MainContent);
---
+++ model.js
@@ -0,0 +1,90 @@
+import { Login, getPageQuery } from './service';
+import CookieUtil from '@/utils/cookie.js';
+import { message } from 'antd';
+
+const Model = {
+  namespace: 'login',
+  state: {},
+
+  // reducers: {
+  //   add_tokens(state, { payload }) {
+  //     return { ...state, tokens: { ...state.tokens, ...payload } };
+  //   },
+  //   clear_tokens(state) {
+  //     return { ...state, tokens: { 1: {}, 2: {} } };
+  //   },
+  // },
+  // reducers: {
+  //   logout() {
+  //     localStorage.removeItem('token');
+  //     localStorage.removeItem('roles');
+  //     // 不是login界面的话跳转到login界面
+  //     if (window.location.pathname !== '/user/login') {
+  //       history.replace({
+  //         pathname: '/user/login',
+  //         search: stringify({
+  //           redirect: window.location.href,
+  //         }),
+  //       });
+  //     }
+  //   },
+  // },
+
+  effects: {
+    *login({ payload }, { call, put }) {
+      const { data, code } = yield call(Login, payload);
+      console.log('back data', data);
+      if (code === 200) {
+        console.log('token', data);
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('roles', data.role_id);
+        console.log('localStorage.token', localStorage.getItem('roles'));
+        // yield put({
+        //   type: 'changeLoginStatus',
+        //   payload: data,
+        // });
+        // const urlParams = new URL(window.location.href);
+        // const params = getPageQuery();
+        // console.log('params', params);
+        // let { redirect } = params;
+        console.log(data);
+        if (data) {
+          return true;
+        } else {
+          return false;
+        }
+        // if (redirect) {
+        //   const redirectUrlParams = new URL(redirect);
+
+        //   if (redirectUrlParams.origin === urlParams.origin) {
+        //     redirect = redirect.substr(urlParams.origin.length);
+
+        //     if (redirect.match(/^\/.*#/)) {
+        //       redirect = redirect.substr(redirect.indexOf('#') + 1);
+        //     }
+        //   } else {
+        //     window.location.href = '/';
+        //     message.success('登录成功！');
+        //     return true;
+        //   }
+        // }
+        // redirect = redirect === 'login' ? '/' : redirect;
+        // history.replace(redirect || '/');
+      }
+    },
+  },
+
+  reducers: {
+    // effect获取数据处理方法
+    changeLoginStatus(state, { payload }) {
+      console.log('payload', payload);
+      localStorage.setItem('token', payload.data.token);
+      localStorage.setItem('roles', payload.data.role_id);
+      console.log('localStorage.token', localStorage.getItem('roles'));
+      // console.log(`login, ${payload.data.role_id}`);
+      return { ...state };
+    },
+  },
+};
+
+export default Model;
