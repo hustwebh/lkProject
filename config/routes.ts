8e8281fb1a19613507e5a755e37d6cb197cdc48e
3115988782@qq.com <3115988782@qq.com>
2022-03-11 00:00:12 +0800

    微调

--- config/routes.ts
+++ config/routes.ts
@@ -1,7 +1,7 @@
 const routerConfig = [
   {
     path: '/user',
-    component: '@/layouts/Userlayout/Userlayouts',
+    // component: '@/layouts/Userlayout/Userlayouts',
     routes: [
       {
         path: '/user/login',
@@ -16,7 +16,7 @@ const routerConfig = [
   {
     path: '/',
     component: '@/layouts/BasicLayout/BasicLayouts',
-    wrappers: ['@/wrappers/auth'],
+    // wrappers: ['@/wrappers/auth'],
     routes: [
       {
         path: '/',
@@ -29,6 +29,10 @@ const routerConfig = [
       {
         path: '/doctorInfo',
         component: '@/pages/doctorInfo/index',
+      },
+      {
+        path: '/infoSearch',
+        component: '@/pages/infoSearch/index',
       },
       {
         path: '/staffManage',
---
+++ src/pages/infoSearch/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import style from './index.less';
+import { Input } from 'antd';
+
+const Index = () => {
+  const { Search } = Input;
+
+  const onSearch = (value: string) => console.log(value);
+
+  return (
+    <div className={style.rightContent}>
+      <div className={style.inputContent}>
+        <Search
+          // maxLength={20}
+          style={{ width: 500 }}
+          placeholder="请输入查询内容"
+          allowClear
+          enterButton="Search"
+          size="large"
+          onSearch={onSearch}
+        />
+      </div>
+    </div>
+  );
+};
+export default Index;
