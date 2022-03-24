828ef5f5272522d26e03672e893a4172029acd8a
‘milerliu’ <1324598012@qq.com>
2022-03-24 22:11:12 +0800

    修改搜索界面

--- src/pages/infoSearch/index.tsx
+++ src/pages/infoSearch/index.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import style from './index.less';
-import { Input } from 'antd';
+// import React from 'react';
+// import style from './index.less';
+// import { Input } from 'antd';
+
+// const Index = () => {
+//   const { Search } = Input;
+
+//   const onSearch = (value: string) => console.log(value);
+
+//   return (
+//     <div className={style.rightContent}>
+//       <div className={style.inputContent}>
+//         <Search
+//           // maxLength={20}
+//           style={{ width: 500 }}
+//           placeholder="请输入查询内容"
+//           allowClear
+//           enterButton="搜索"
+//           size="large"
+//           onSearch={onSearch}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+// export default Index;
+import React, { useState } from 'react';
+import { Form, Row, Col, Input, Button, Select } from 'antd';
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+
+const { Option } = Select;
 
 const Index = () => {
-  const { Search } = Input;
+  const [form] = Form.useForm();
 
-  const onSearch = (value: string) => console.log(value);
+  const getFields = () => {
+    const count = 6;
+    const children = [];
+    for (let i = 0; i < count; i++) {
+      children.push(
+        <Col span={8} key={i}>
+          <Form.Item
+            name={`field-${i}`}
+            label={`Field ${i}`}
+            rules={[
+              {
+                required: true,
+                message: 'Input something!',
+              },
+            ]}
+          >
+            <Input placeholder="placeholder" />
+            {i % 3 !== 1 ? (
+              <Input placeholder="placeholder" />
+            ) : (
+              <Select defaultValue="2">
+                <Option value="1">1</Option>
+                <Option value="2">
+                  longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong
+                </Option>
+              </Select>
+            )}
+          </Form.Item>
+        </Col>,
+      );
+    }
+    return children;
+  };
+
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
 
   return (
-    <div className={style.rightContent}>
-      <div className={style.inputContent}>
-        <Search
-          // maxLength={20}
-          style={{ width: 500 }}
-          placeholder="请输入查询内容"
-          allowClear
-          enterButton="搜索"
-          size="large"
-          onSearch={onSearch}
-        />
-      </div>
-    </div>
+    <Form
+      form={form}
+      name="advanced_search"
+      className="ant-advanced-search-form"
+      onFinish={onFinish}
+    >
+      <Row gutter={24}>{getFields()}</Row>
+      <Row>
+        <Col span={24} style={{ textAlign: 'right' }}>
+          <Button type="primary" htmlType="submit">
+            Search
+          </Button>
+          <Button
+            style={{ margin: '0 8px' }}
+            onClick={() => {
+              form.resetFields();
+            }}
+          >
+            Clear
+          </Button>
+        </Col>
+      </Row>
+    </Form>
   );
 };
+
 export default Index;
