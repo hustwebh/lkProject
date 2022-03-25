cb02cd17a158df945106540a2bc761b849cff372
3115988782@qq.com <3115988782@qq.com>
2022-03-25 12:27:07 +0800

    侧边栏显示修复

--- src/layouts/BasicLayout/service.ts
+++ src/layouts/BasicLayout/service.ts
@@ -4,18 +4,18 @@ import { SERVICEURL } from '@/utils/const';
 export async function getLoginUserMsg() {
   return request(`${SERVICEURL}/api/v1/user`, {
     method: 'GET',
-    headers: {
-      token: localStorage.getItem('token'),
-    },
+    // headers: {
+    //   token: localStorage.getItem('token'),
+    // },
   });
 }
 
 export async function getHospitalList() {
   return request(`${SERVICEURL}/api/v1/hospitalAll`, {
     method: 'GET',
-    headers: {
-      token: localStorage.getItem('token'),
-    },
+    // headers: {
+    //   token: localStorage.getItem('token'),
+    // },
   });
 }
 
@@ -23,16 +23,16 @@ export async function getPatientMsg(body: any) {
   return request(`${SERVICEURL}/api/v1/patient`, {
     method: 'GET',
     body,
-    headers: {
-      token: localStorage.getItem('token'),
-    },
+    // headers: {
+    //   token: localStorage.getItem('token'),
+    // },
   });
 }
 export async function getMainDoctorMsg(body: any) {
   return request(`${SERVICEURL}/api/v1/user/${body.doctor_id}`, {
     method: 'GET',
-    headers: {
-      token: localStorage.getItem('token'),
-    },
+    // headers: {
+    //   token: localStorage.getItem('token'),
+    // },
   });
 }
